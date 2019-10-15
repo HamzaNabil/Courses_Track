42364858847cb9c7db3afe615d8b73ce3b15bdf9
HamzaNabil <hamzanabil342@yahoo.com>
2019-10-15 21:08:59 +0200

    some edits

---
+++ js/data.js
@@ -0,0 +1,17 @@
+[
+    {
+        "First": {
+            "title": "First Step <span> => </span> Learn HTML/5, Css/3, Bootstrap & Create Templates", 
+            "details": {
+                "items": [
+                    {
+                        "id": "1", 
+                        "courseTitle": "How To Start",
+                        "courseDescription": "How to Start & what U should learn.",
+                        "courseLink": "https://www.youtube.com/playlist?list=PLtFbQRDJ11kH_lzWv3SCRzKcF5MarNu_1"
+                    }
+                ]
+              }
+        }
+    }
+]
--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
 
-    
+    $.ajax({
+        url: 'js/data.js',
+        type: "GET",
+        success: function(res) {
+            const result = JSON.parse(res);
+            result.map( item => {
+                console.log(item.First)
+            })
+        },
+        error: function(err) {
+            console.log(err)
+        }
+    })
+
+    $(".channel-track + .icon-down").on('click', function() {
+        $("html,body").animate({
+            scrollTop: $('.wrapper').offset().top
+        }, 600)
+    })
+
+    $(".new-videos-alert").on('click', function() {
+        $(".new-videos").removeClass('hide')
+        $(".new-videos").addClass('toggle');
+    })
+
+    $(".new-videos .close-list").on('click', function() {
+        $(".new-videos").addClass('hide');
+    })
 
 })
