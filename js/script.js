4c117c1c86d793a4fad12c10ecdecb323e5f6881
HamzaNabil <hamzanabil342@yahoo.com>
2019-10-19 19:40:02 +0200

    edito

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $.ajax({
+    url: "js/data.js",
+    type: "GET",
+    success: function(res) {
+      const result = JSON.parse(res);
+      result.map(item => {
+        // console.log(item.First)
+      });
+    },
+    error: function(err) {
+      // console.log(err)
+    }
+  });
 
-    $.ajax({
-        url: 'js/data.js',
-        type: "GET",
-        success: function(res) {
-            const result = JSON.parse(res);
-            result.map( item => {
-                console.log(item.First)
-            })
-        },
-        error: function(err) {
-            console.log(err)
-        }
-    })
+  $(".channel-track + .icon-down").on("click", function() {
+    $("html,body").animate(
+      {
+        scrollTop: $(".wrapper").offset().top
+      },
+      600
+    );
+  });
 
-    $(".channel-track + .icon-down").on('click', function() {
-        $("html,body").animate({
-            scrollTop: $('.wrapper').offset().top
-        }, 600)
-    })
+  $(".new-videos-alert").on("click", function() {
+    $(".new-videos").removeClass("hide");
+    $(".new-videos").addClass("toggle");
+  });
 
-    $(".new-videos-alert").on('click', function() {
-        $(".new-videos").removeClass('hide')
-        $(".new-videos").addClass('toggle');
-    })
-
-    $(".new-videos .close-list").on('click', function() {
-        $(".new-videos").addClass('hide');
-    })
-
-})
+  $(".new-videos .close-list").on("click", function() {
+    $(".new-videos").addClass("hide");
+  });
+});
